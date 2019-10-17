08a00fb1c29213765bea01699bf89c2219c9cc7f
Bernard Wong <bew030@ucsd.edu>
2019-10-17 01:32:21 -0700

    added background and animations

--- dino_jump/dino_jump_remake.py
+++ dino_jump/dino_jump_remake.py
@@ -2,24 +2,55 @@ import pygame
 
 pygame.init() # initializing pygame, necessary, in general do at the beginning 
 
-game_border_x = 500
+walkRight = [pygame.image.load('sprite/R'+str(x)+'.png') for x in range(1,10)] 
+walkLeft = [pygame.image.load('sprite/L'+str(x)+'.png') for x in range(1,10)]
+bg = pygame.image.load('bg.png')
+char = pygame.image.load('standing.png')
+
+clock = pygame.time.Clock()
+
+game_border_x = 1000
 game_border_y = 500 
-x = 59 
-y = 300 
-width = 40 
-height = 60 
+x = 50 
+y = 385
+width = 64
+height = 64 
 velocity = 5
 
 window = pygame.display.set_mode((game_border_x,game_border_y)) # setting display window, (width, height)
-pygame.display.set_caption('test') # naming the game window 
+pygame.display.set_caption('Dino Jump') # naming the game window 
 
 isJump = False 
 jumpCount = 10 
 realistic = True
+
+# need to keep track of what direction character is moving, are they moving, how many steps theyve moved 
+left = False 
+right = False 
+walkCount = 0 # gives you positioning of sprite in list 
+
+
+def redrawGameWindow():
+    global walkCount 
+    
+    #window.fill((0,0,0)) # black background
+    window.blit(bg, (0,0)) # filling with background image 
+    if (walkCount + 1) >= 27: # 9 sprites, each sprite for 3 frames 
+        walkCount = 0 # pygame.draw.rect(window,(255,255,255),(x,y,width,height)) # look at website to see what shapes to draw (window, color, (x,y,width,height))
+    if left: 
+        window.blit(walkLeft[walkCount//3],(x,y)) 
+        walkCount += 1 
+    elif right: 
+        window.blit(walkRight[walkCount//3],(x,y))
+        walkCount += 1
+    else: 
+        window.blit(char,(x,y))
+    pygame.display.update() # updates screen # can use pygame.display.flip() to update whole screen, but typically slower
+
 # main loop 
 run = True 
 while run:
-    pygame.time.delay(15) # milleseconds 
+    clock.tick(27) # pygame.time.delay(27) # milleseconds 
     for event in pygame.event.get(): # get a list of all the events that happen 
         if event.type == pygame.QUIT: 
             run = False 
@@ -27,25 +58,27 @@ while run:
     keys = pygame.key.get_pressed() 
     if keys[pygame.K_LEFT]:
         x -= velocity 
+        left = True 
+        right = False 
         if x < 0: # setting game boundary 
             x = 0
-    if keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_RIGHT]:
         x += velocity 
+        left = False 
+        right = True
         if x > (game_border_x-width): # setting game boundary 
             x = game_border_x-width
-    '''        
-    if keys[pygame.K_UP]:
-        y -= velocity
-        if y < 0: # setting game boundary 
-            y = 0
-    if keys[pygame.K_DOWN]:
-        y += velocity
-        if y > (game_border_y-height): # setting game boundary
-            y = (game_border_y-height)
-    '''
+    else: 
+        left = False
+        right = False
+        walkCount = 0 # what's the point of that? 
+
     if not isJump:
         if keys[pygame.K_SPACE]:
             isJump = True
+            right = False 
+            left = False
+            walkCount = 0 
     else:  
         if jumpCount >= -10:
             if realistic: 
@@ -63,8 +96,6 @@ while run:
             isJump = False  
             jumpCount = 10 
 
-    window.fill((0,0,0))
-    pygame.draw.rect(window,(255,0,0),(x,y,width,height)) # look at website to see what shapes to draw (window, color, (x,y,width,height)) # how come this goes after?
-    pygame.display.update() # updates screen # can use pygame.display.flip() to update whole screen, but typically slower 
+    redrawGameWindow()
 
 pygame.quit()
